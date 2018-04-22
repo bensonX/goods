1f32353fa11bb85da49845d38328ac4fba50e80c
sxl <1017420146@qq.com>
2018-04-22 14:21:30 +0800

    aaa

--- goods.java
+++ goods.java
@@ -30,5 +30,8 @@ public class StatusBarUtil {
     private static final int FAKE_STATUS_BAR_VIEW_ID = R.id.statusbarutil_fake_status_bar_view;
     private static final int FAKE_TRANSLUCENT_VIEW_ID = R.id.statusbarutil_translucent_view;
     private static final int TAG_KEY_HAVE_SET_OFFSET = -123;
- 
+    
+    shangxianlou  修改
+
+
 }
