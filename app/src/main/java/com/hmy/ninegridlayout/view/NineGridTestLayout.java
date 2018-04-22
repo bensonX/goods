d9d112b3d193624cb7c3c583c2d5712fe69c52bd
sxl <1017420146@qq.com>
2018-04-22 18:14:55 +0800

    fix

--- app/src/main/java/com/hmy/ninegridlayout/view/NineGridTestLayout.java
+++ app/src/main/java/com/hmy/ninegridlayout/view/NineGridTestLayout.java
@@ -41,7 +41,7 @@ public class NineGridTestLayout extends NineGridLayout {
 
 
 
-
+        //开始修改了，修改数据中
         Glide.with(mContext).load(url).asBitmap().skipMemoryCache(false).diskCacheStrategy(DiskCacheStrategy.ALL).into(new SimpleTarget<Bitmap>() {
             @Override
             public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
