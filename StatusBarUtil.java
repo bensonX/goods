f2b7e262e5d83b8b9540fef5afa38b4c1e535473
sxl <1017420146@qq.com>
2018-04-22 14:46:54 +0800

    aa

--- StatusBarUtil.java
+++ StatusBarUtil.java
@@ -30,6 +30,9 @@ public class StatusBarUtil {
     private static final int FAKE_STATUS_BAR_VIEW_ID = R.id.statusbarutil_fake_status_bar_view;
     private static final int FAKE_TRANSLUCENT_VIEW_ID = R.id.statusbarutil_translucent_view;
     private static final int TAG_KEY_HAVE_SET_OFFSET = -123;
+  
+    master 分支
+
 
     /**
      * 设置状态栏颜色
